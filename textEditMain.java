c9f7c6f2db8fdf471244054306c84b781be2d58f
kcm2019 <65182311+kcm2019@users.noreply.github.com>
2020-05-30 16:10:35 -0400

    Add files via upload

---
+++ textEditMain.java
@@ -0,0 +1,30 @@
+import javax.swing.*;    
+import java.awt.event.*;    
+import javax.swing.JFrame;
+
+public class textEditMain
+{
+    private static void createAndShowGUI() 
+    {
+
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        JFrame frame = new JFrame("Text Editor");
+
+        //Create and set up the content pane.
+        TextEditor demo = new TextEditor();
+        frame.setContentPane(demo.createTextEditor());
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 450);
+        frame.setVisible(true);
+    }  
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
